4819d93a125515ef28e9d79c9db24e090ab742d1
DESKTOP-82DHVBJ\Albert <albert.benavent.cabrera@gmail.com>
2021-06-05 00:43:19 +0200

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # map-markers_React
- map with creation of markers through suggestions
+ Use of themoviedb.org to obtain the data. Grid view with back and next. Favorites menu.
+ 
+ 
+1. Download
+2. Place in the Movies_Hooks folder and put -> npm install to install all the dependencies.
+3. In the same folder put -> npm start.
+
+
+<div classname="center">
+<img src="https://i.ibb.co/rw3tXXW/pruebafront.jpg" >
+</div>
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Map } from "./pages/page1/Map.js";
+import { Provider } from "react-redux";
+import store from "./app/store";
+ReactDOM.render(
+  <Provider store={store}>
+    <Map />
+  </Provider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
